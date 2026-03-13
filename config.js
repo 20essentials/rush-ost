aa814d0691f0ff907b0e71ff34df36fbb528df1c
DominicDev <s0l0estudi4r@gmail.com>
2026-03-13 17:46:08 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'RUSH';
+export let nameSongs = [
+	"YYZ", /*1*/
+	"SPIRIT OF RADIO", /*2*/
+	"SUBDIVISIONS", /*3*/
+	"LIMELIGHT", /*4*/
+	"TOM SAWYER", /*5*/
+	"WORKING MAN", /*6*/
+	"FLY BY NIGHT", /*7*/
+	"ANTHEM", /*8*/
+	"FINDING MY WAY", /*9*/
+	"HERE AGAIN", /*10*/
+	"WHAT YOU'RE DOING", /*11*/
+	"CHEMISTRY", /*12*/
+	"DIGITAL MAN", /*13*/
+	"COUNTDOWN", /*14*/
+	"TIME STAND STILL", /*15*/
+	"CLOSER TO THE HEART", /*16*/
+	"FREEWILL", /*17*/
+	"NATURAL SCIENCE", /*18*/
+	"WITCH HUNT", /*19*/
+	"VITAL SIGNS", /*20*/
+]
